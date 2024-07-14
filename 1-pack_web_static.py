cd9b8263d145efa0d6dcd790a8e0eb4878f133a2
Monarch <monarchofaether@gmail.com>
2024-07-14 17:28:27 +0300

    fabric3==1.14.post1 used

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
 """ Uses fabric to compress web_static files
 """
-from fabric import task
+from fabric.api import local
 import os
 from datetime import datetime
 
-
-@task
-def do_pack(c):
+def do_pack():
     """ Packs the web_static files into the versions directory
     named according to date created.
     """
     if not os.path.exists("versions"):
-        c.run("mkdir versions")
+        local("mkdir versions")
     date = datetime.now().strftime('%Y%m%d%H%M%S')
     folder_to_archive = 'web_static'
     output_file = 'versions/webstatic_{}.tgz'.format(date)
-    result = c.run("tar -czf {} -v {}".format(output_file, folder_to_archive),
-                   pty=True, echo=True)
+    result = local("tar -czf {} -v {}".format(output_file, folder_to_archive))
 
     if result.ok:
         return output_file
