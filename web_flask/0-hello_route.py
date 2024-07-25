bb2c2da9367c5bcae7fd67d50106c8f9fa6040bb
Monarch <monarchofaether@gmail.com>
2024-07-25 17:44:29 +0300

    first flask web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+from flask import Flask
+""" Module using flask to print Hello message
+"""
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """ Prints Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
