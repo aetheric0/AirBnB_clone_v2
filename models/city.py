ac71229b3c89df651e2a4fc9e0e7601b12df3b59
Monarch <monarchofaether@gmail.com>
2024-07-26 11:31:45 +0300

    User class created

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+#from models.place import Place
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -10,3 +11,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+#    places = relationship('Place', back_populates='cities')
---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Adding a route with a variable name
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ Prints Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb/', strict_slashes=False)
+def hbnb():
+    """ Prints HBNB
+    """
+    return "HBNB"
+
+@app.route('/c/<text>/', strict_slashes=False)
+def c_is_fun(text):
+    """ Displays "C " followed by string in <text>
+    """
+    return "C {}".format(text)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
